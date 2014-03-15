068ba68dd1c853447685f4c0bee9ce0582e53e3a
Ram Nadella <ram.nadella@live.com>
2014-03-15 03:18:27 -0700

    Initial commit. Keep on surfin...

---
+++ js/app.js
@@ -0,0 +1,38 @@
+document.write('node ' + process.version);
+
+$digOutput = $('#digOutput');
+$nsOutput = $('#nsOutput');
+
+clearAllOutputs = function() {
+	$digOutput.html('');
+	$nsOutput.html('');
+};
+
+$('#surf').click(function(e) {
+
+	e.preventDefault();
+
+	NProgress.start();
+
+	domainName = $('#domainName').val();
+
+	clearAllOutputs();
+
+	// run dig and write output
+	var spawn = require('child_process').spawn,
+		dig = spawn('dig', [domainName]),
+		ns = spawn('dig', ['NS', '+short', domainName]);
+
+	dig.stdout.on('data', function (data) {
+		$digOutput.append(data.toString());
+	});
+
+	ns.stdout.on('data', function (data) {
+		$nsOutput.append(data.toString());
+	});
+
+	dig.on('close', function() {
+		NProgress.done();
+	});
+
+});
